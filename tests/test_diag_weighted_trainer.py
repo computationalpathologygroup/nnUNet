c203a66fa0f7f04e6d2caff2edfa00ee8095bafa
silvandeleemput <silvandeleemput@gmail.com>
2022-01-10 16:58:27 +0100

    Added bootstrap/check_integrity methods for weightmap test files, added better augmentation test #7
    * Applied black formatting

--- tests/test_diag_weighted_trainer.py
+++ tests/test_diag_weighted_trainer.py
@@ -1,32 +1,34 @@
-from collections import OrderedDict
+import pickle
 
+from collections import OrderedDict
+from typing import Dict
+from pathlib import Path
+
+import pytest
+import torch
+import numpy as np
+
+from batchgenerators.transforms.channel_selection_transforms import (
+    SegChannelSelectionTransform,
+)
 from batchgenerators.transforms.color_transforms import GammaTransform
 from batchgenerators.transforms.utility_transforms import (
     RenameTransform,
     RemoveLabelTransform,
     NumpyToTensor,
 )
-from typing import Dict
-
-import pickle
-import pytest
-import torch
-from batchgenerators.transforms.channel_selection_transforms import (
-    SegChannelSelectionTransform,
-)
-
-from pathlib import Path
 
 import nnunet
-from nnunet.training.data_augmentation.downsampling import DownsampleSegForDSTransform2
+from nnunet.training.data_augmentation.downsampling import (
+    DownsampleSegForDSTransform2,
+    downsample_seg_for_ds_transform2,
+)
 from nnunet.training.dataloading.dataset_loading import (
     load_dataset,
     unpack_dataset,
     DataLoader3D,
     DataLoader2D,
 )
-import numpy as np
-
 from nnunet.training.loss_functions.dice_loss import DC_and_CE_loss
 from nnunet.training.network_training.diag.dataset_loading import DataLoader3DWeighted
 from nnunet.training.network_training.diag.nnUNetTrainerV2Weighted import (
@@ -37,23 +39,61 @@ from nnunet.training.network_training.diag.transforms.spatial_transforms import 
     SpatialTransformWithWeights,
     MirrorTransformWithWeights,
 )
+import nnunet.utilities.task_name_id_conversion as nnp
+import nnunet.run.default_configuration as nndc
+from nnunet.paths import default_plans_identifier
+from nnunet.utilities.diag.generate_unet_weightmaps import (
+    generate_unet_weightmaps,
+    auto_detect_classes,
+)
+
 from tests.test_training import check_expected_training_output, prepare_paths
+from tests.utils import is_data_integrity_ok_md5sum
 
 RESOURCES_DIR = Path(__file__).parent / "resources"
 NNUNET_PREPROCESSING_DATA_DIR = RESOURCES_DIR / "nnUNet" / "nnUNet_preprocessed_data"
 HIPPOCAMPUS_TASK_ID = 4
+HIPPOCAMPUS_TASK = "Task004_Hippocampus"
 
-import nnunet.utilities.task_name_id_conversion as nnp
-import nnunet.run.default_configuration as nndc
-from nnunet.paths import default_plans_identifier
+
+def test_bootstrapping_weightmaps(tmp_path):
+    input_dir = (
+        NNUNET_PREPROCESSING_DATA_DIR
+        / HIPPOCAMPUS_TASK
+        / "nnUNetData_plans_v2.1_stage0"
+    )
+    output_dir = Path(tmp_path / HIPPOCAMPUS_TASK / "nnUNetData_plans_v2.1_stage0")
+    output_dir.mkdir(parents=True)
+    matching_pattern = "*.npz"
+    classes = auto_detect_classes(
+        input_dir=input_dir, matching_pattern=matching_pattern
+    )
+    generate_unet_weightmaps(
+        input_dir=input_dir,
+        output_dir=output_dir,
+        matching_pattern=matching_pattern,
+        sigma=5.0,
+        w_0=10.0,
+        classes=classes,
+    )
+    assert is_data_integrity_ok_md5sum(
+        workdir=tmp_path,
+        md5file=NNUNET_PREPROCESSING_DATA_DIR / "Task004_Hippocampus_weightmaps.md5",
+    )
 
 
 def test_dataset_loading():
-    task = "Task004_Hippocampus"
-    p = NNUNET_PREPROCESSING_DATA_DIR / task / "nnUNetData_plans_v2.1_stage0"
+    p = (
+        NNUNET_PREPROCESSING_DATA_DIR
+        / HIPPOCAMPUS_TASK
+        / "nnUNetData_plans_v2.1_stage0"
+    )
     dataset = load_dataset(str(p))
     with open(
-        NNUNET_PREPROCESSING_DATA_DIR / task / "nnUNetPlansv2.1_plans_3d.pkl", "rb"
+        NNUNET_PREPROCESSING_DATA_DIR
+        / HIPPOCAMPUS_TASK
+        / "nnUNetPlansv2.1_plans_3d.pkl",
+        "rb",
     ) as f:
         plans = pickle.load(f)
     patch_size = plans["plans_per_stage"][0]["patch_size"]
@@ -134,8 +174,11 @@ def test_weighted_data_augmentation():
         "num_cached_per_thread": 2,
         "patch_size_for_spatialtransform": np.array([40, 56, 40]),
     }
-    task = "Task004_Hippocampus"
-    p = NNUNET_PREPROCESSING_DATA_DIR / task / "nnUNetData_plans_v2.1_stage0"
+    p = (
+        NNUNET_PREPROCESSING_DATA_DIR
+        / HIPPOCAMPUS_TASK
+        / "nnUNetData_plans_v2.1_stage0"
+    )
     dataset = load_dataset(str(p))
     dl_tr = DataLoader3DWeighted(
         dataset,
@@ -258,11 +301,24 @@ def test_weighted_data_augmentation():
             assert all([key in data_dict for key in ["data", "target", "weightmap"]])
             assert "seg" not in data_dict
             assert_data_properties(data_dict=data_dict, seg_key="target", seg_min=0.0)
+            seg_ref = np.copy(data_dict["target"])
+            weightmap_ref = np.copy(data_dict["weightmap"])
         elif idx == 14:
             assert isinstance(tf, DownsampleSegForDSTransform2)
-            for key in ["target", "weightmap"]:
+            for order, (img, key) in enumerate(
+                zip([seg_ref, weightmap_ref], ["target", "weightmap"])
+            ):
                 assert isinstance(data_dict[key], list)
                 assert len(data_dict[key]) == 3
+                for idx, scales in enumerate(
+                    ((1.0, 1.0, 1.0), (0.5, 0.5, 0.5), (0.25, 0.25, 0.25))
+                ):
+                    assert np.array_equal(
+                        data_dict[key][idx],
+                        downsample_seg_for_ds_transform2(
+                            seg=img, ds_scales=(scales,), order=order, axes=None,
+                        )[0],
+                    )
         elif idx == 15:
             assert isinstance(tf, NumpyToTensor)
             assert isinstance(data_dict["data"], torch.Tensor)
@@ -295,12 +351,15 @@ def test_weighted_trainer(tmp_path: Path, network: str, fold: int):
         stage,
         trainer_class,
     ) = nndc.get_default_configuration(
-        network, task, "nnUNetTrainerV2Weighted", default_plans_identifier
+        network=network,
+        task=task,
+        network_trainer=nnUNetTrainerV2Weighted.__name__,
+        plans_identifier=default_plans_identifier,
     )
     assert issubclass(trainer_class, nnUNetTrainerV2Weighted)
     trainer = trainer_class(
-        plans_file,
-        fold,
+        plans_file=plans_file,
+        fold=fold,
         output_folder=output_folder_name,
         dataset_directory=dataset_directory,
         batch_dice=batch_dice,
@@ -324,7 +383,7 @@ def test_weighted_trainer(tmp_path: Path, network: str, fold: int):
     check_expected_training_output(
         check_dir=tmp_path,
         network=network,
-        trainer_class_name="nnUNetTrainerV2Weighted",
+        trainer_class_name=nnUNetTrainerV2Weighted.__name__,
     )
 
 
